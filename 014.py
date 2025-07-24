3d81ceba2fba6508c9fc2ec936256ac74d817e8a
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-07-24 21:31:05 +0530

    Update 014.py

--- 014.py
+++ 014.py
@@ -1,3 +1,7 @@
+''' Chef’s brain works correctly up to X bps. If he is currently working at Y bps, print YES if he is prone to errors (Y > X), otherwise print NO.
+Input: Two space-separated integers X and Y
+Output: Print YES or NO '''
+
 # Input: two space-separated integers
 X, Y = map(int, input().split())
 
