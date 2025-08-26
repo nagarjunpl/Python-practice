a6d4f36580f8d6b9132ae004e94fcb6911cf54cb
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-08-26 13:37:03 +0530

    Create 026.py

---
+++ 026.py
@@ -0,0 +1,8 @@
+# Convert a given string into lowercase letters.
+
+class Solution:
+    def toLower(self, s: str) -> str:
+        # Convert the string to lowercase using Python's built-in method
+        convert = s.lower()
+        # Return the converted string
+        return convert
