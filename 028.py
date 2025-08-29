60982a7ef35d8a06a0f21606444b8eed5ab7ad37
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-08-29 16:00:33 +0530

    Create 028.py

---
+++ 028.py
@@ -0,0 +1,13 @@
+# Q: What will be the output of comparing these two strings?
+
+s1 = "apple"
+s2 = "banana"
+
+# "apple" is not equal to "banana", therefore it is False
+print(s1 == s2)
+
+# "apple" is different from "banana", therefore it is True
+print(s1 != s2)
+
+# "apple" comes before "banana" lexicographically, therefore it is True
+print(s1 < s2)
