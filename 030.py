595d47f1b67a97fe42493211abb6bc6d4802fc47
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-08-29 16:08:43 +0530

    Create 030.py

---
+++ 030.py
@@ -0,0 +1,13 @@
+# Q: What will be the output of converting "1010" from binary to decimal?
+
+# Binary string
+s = "1010"
+num = int(s, 2)
+print(num)   
+
+# Q: What will be the output of converting "A" from hexadecimal to decimal?
+
+# Hexadecimal string
+s = "A"
+num = int(s, 16)
+print(num)   
