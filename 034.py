e05eb137e1fd67dc3c6fd3a1344d2ae41a8e8f2d
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-10-06 17:53:03 +0530

    Create 034.py

---
+++ 034.py
@@ -0,0 +1,19 @@
+class Solution:
+    def whichWeekDay(self, day):
+        match day:
+            case 1 :
+                print("Monday")
+            case 2 :
+                print("Tuesday")
+            case 3 :
+                print("Wednesday")
+            case 4 :
+                print("Thursday")
+            case 5 :
+                print("Friday")
+            case 6 :
+                print("Saturday")
+            case _ :
+                print("Invalid")
+
+a = Solution()
