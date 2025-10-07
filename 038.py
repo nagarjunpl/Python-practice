23b0fb9d60b6d19b9831f5deec8b97b8d85a9968
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-10-07 20:27:07 +0530

    Create 038.py

---
+++ 038.py
@@ -0,0 +1,13 @@
+# Count all Digits of a Number
+
+class Solution:
+    def countDigit(self, n):
+        count = len(str(n))
+        print(count)
+
+a = Solution()
+a.countDigit(598)
+
+'''
+OUTPUT: 3
+'''
