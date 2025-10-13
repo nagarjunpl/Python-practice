ba91052c29e9e23fb0ddd2491c82910537cfd315
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-10-13 17:03:17 +0530

    Update 042.py

--- 042.py
+++ 042.py
@@ -10,6 +10,6 @@ class Solution:
       
 a = Solution()
 
-print(a.divisors(8))   
-print(a.divisors(12))  
-print(a.divisors(25))  
+print(a.divisors(8))   # Output: [1, 2, 4, 8]
+print(a.divisors(12))  # Output: [1, 2, 3, 4, 6, 12]
+print(a.divisors(25))  # Output: [1, 5, 25]
